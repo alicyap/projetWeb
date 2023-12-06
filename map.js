55e75d9c961fadbe1ad4a50e4d9b85e9689b70a4
Fsv6 <112781646+Fsv6@users.noreply.github.com>
2023-12-06 18:30:06 +0100

    changement de données et créations fonction pour les marqueurs

--- map.js
+++ map.js
@@ -1,4 +1,9 @@
-var mymap = L.map('map').setView([46.6031, 1.8883], 6); //carte européano-centrée (France)
+var mymap = L.map('map').setView([46.6031, 1.8883], 6);
+
+var tabname = new Set();
+var tabCodeUIC = new Set();
+var latLongGare = [];
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -8,6 +13,57 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 $( "#interet" ).selectmenu(
     {width: 250,
   icons: { button: "ui-icon-caret-1-s" }});
+
+//récupère dans le json les noms des villes dans lesquelles sont nos gares
+fetch('referentiel-gares-voyageurs.json')
+    .then(response => response.json())
+    .then(data => {
+
+    data.forEach(objet => {
+        const nomGare = objet.alias_libelle_noncontraint;
+        const codeUIC = objet.uic_code;
+        const longLat = objet.wgs_84;
+
+
+
+        if (!tabname.has(nomGare)) {
+            tabname.add(nomGare);
+            tabCodeUIC.add(codeUIC);
+            latLongGare.push(longLat);
+
+        }
+        console.log(latLongGare);
+    });
+
+        createMarkeur(latLongGare);
+})
+    .catch(error => {
+        console.error('Erreur lors de la récupération du fichier JSON:', error);
+    });
+
+function createMarkeur(tab) {
+    tab.forEach(coordonnees => {
+        const latitude = coordonnees.lat;
+        const longitude = coordonnees.lon;
+        L.marker([latitude, longitude]).addTo(mymap)
+            .bindPopup('CC')
+            .openPopup();
+    });
+}
+
+
+
+/*document.addEventListener("DOMContentLoaded", function() {
+
+    console.log(latGare);
+
+    // Appelez la fonction createMarkeur avec vos ensembles de latitude et de longitude
+    createMarkeur(latGare, longGare);
+});
+
+
+
+
 
 /*L.marker([51.5, -0.09]).addTo(mymap)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
